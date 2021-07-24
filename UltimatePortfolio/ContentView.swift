48b5e17b72e53f8bef7f27481545d643b624d733
Lorand Fazakas <lorando.fazakas@gmail.com>
2021-07-24 15:24:58 +0200

    first commit

---
+++ UltimatePortfolio/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by Lorand Fazakas on 2021. 07. 19..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+            ProjectsView(showClosedProjects: true)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Closed")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
+  
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,26 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Lorand Fazakas on 2021. 07. 19..
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
