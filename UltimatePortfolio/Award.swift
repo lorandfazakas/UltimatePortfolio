44d66f3229ab0d6d3d0ab324d717f951ca973244
Lorand Fazakas <lorando.fazakas@gmail.com>
2021-07-29 10:00:35 +0200

    Created Awards and Home view

---
+++ UltimatePortfolio/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  UltimatePortfolio
+//
+//  Created by Lorand Fazakas on 2021. 07. 27..
+//
+
+import Foundation
+
+struct Award: Identifiable, Decodable {
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Lorand Fazakas on 2021. 07. 28..
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
