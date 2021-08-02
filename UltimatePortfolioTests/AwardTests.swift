d707c68f6fc08a73f289fb50db0e0ab72f1066a6
Lorand Fazakas <lorando.fazakas@gmail.com>
2021-08-02 18:16:17 +0200

    fixed issue that multiple DataController instance loaded the CoreData models everytime causing ambiguity + changed Project and Item example to use preview DataController to persist after the return + development tests added

--- UltimatePortfolioTests/AwardTests.swift
+++ UltimatePortfolioTests/AwardTests.swift
@@ -28,22 +28,15 @@ class AwardTests: BaseTestCase {
         let values = [1, 10, 20, 50, 100, 250, 500, 1000]
 
         for (count, value) in values.enumerated() {
-            var items = [Item]()
-
             for _ in 0..<value {
-                let item = Item(context: managedObjectContext)
-                items.append(item)
+                _ = Item(context: managedObjectContext)
             }
-
             let matches = awards.filter { award in
                 award.criterion == "items" && dataController.hasEarned(award: award)
             }
 
             XCTAssertEqual(matches.count, count + 1, "Adding \(value) items should unlock \(count + 1) awards.")
-
-            for item in items {
-                dataController.delete(item)
-            }
+            dataController.deleteAll()
         }
 
     }
@@ -52,12 +45,9 @@ class AwardTests: BaseTestCase {
         let values = [1, 10, 20, 50, 100, 250, 500, 1000]
 
         for (count, value) in values.enumerated() {
-            var items = [Item]()
-
             for _ in 0..<value {
                 let item = Item(context: managedObjectContext)
                 item.completed = true
-                items.append(item)
             }
 
             let matches = awards.filter { award in
@@ -65,10 +55,7 @@ class AwardTests: BaseTestCase {
             }
 
             XCTAssertEqual(matches.count, count + 1, "Completing \(value) items should unlock \(count + 1) awards.")
-
-            for item in items {
-                dataController.delete(item)
-            }
+            dataController.deleteAll()
         }
 
     }
