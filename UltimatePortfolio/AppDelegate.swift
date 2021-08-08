849f29582bb49822fa6f160a8a38f456cecbf4b3
Lorand Fazakas <lorando.fazakas@gmail.com>
2021-08-08 13:33:51 +0200

    Adding a quick action to create new project

---
+++ UltimatePortfolio/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  UltimatePortfolio
+//
+//  Created by Lorand Fazakas on 2021. 08. 08..
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
+    }
+}
